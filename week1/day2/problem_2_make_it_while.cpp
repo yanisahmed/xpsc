62eb94469c9f0255478b9a0a81253bca7e2c4093
Yanis Ahmed <yanisahmed12@gmail.com>
2024-10-28 15:05:03 +0600

    solved

--- week1/day2/problem_2_make_it_while.cpp
+++ week1/day2/problem_2_make_it_while.cpp
@@ -17,6 +17,40 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+
+        int spos = 0;
+        int epos = n - 1;
+
+        while(n > 0)
+        {
+           if(s[spos] == 'B')
+           {
+               break;
+           }
+           spos++;
+        }
+
+        while(n > 0)
+        {
+           if(s[epos] == 'B')
+           {
+               break;
+           }
+           epos--;
+        }
+        //cout<<spos<<" "<<epos<<endl;
+        cout<<epos - spos + 1 <<endl;
+
+    }
+
 
     return 0;
 }
