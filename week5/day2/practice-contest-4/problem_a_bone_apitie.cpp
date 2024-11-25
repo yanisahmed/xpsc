eb4bc8bbb197f8cf2f856377780cf3aeb870c1c2
Yanis Ahmed <yanisahmed12@gmail.com>
2024-11-25 21:55:46 +0600

    practice contest 4 added

---
+++ week5/day2/practice-contest-4/problem_a_bone_apitie.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+
+    int n,m,x,y;
+    cin>>n>>m>>x>>y;
+    cout<< (n*x) + (m*y)<<endl;
+    return 0;
+}
