89d07a9be2fe3835abcc9a4797f3678b1e5e8538
Yanis Ahmed <yanisahmed12@gmail.com>
2024-11-28 22:55:26 +0600

    chemetry problem

--- week5/day5/chemistry.cpp
+++ week5/day5/chemistry.cpp
@@ -1,27 +1,33 @@
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main()
-{
+void solve(int tc) {
+   int n,k;
+   string s;
+   cin>>n>>k>>s;
+   map<char,int>mp;
+   for(int i = 0;i<s.size();i++)mp[s[i]]++;
+
+   int cur = 0;
+   for(auto i : mp)
+   {
+       if(i.second & 1)cur++;
+   }
+   if(cur)cur--;
+
+   k-=cur;
+   if(k>=0 || k%2==0 )cout<<"YES"<<endl;
+   else cout<<"NO"<<endl;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int n, k;
-        cin>>n>>k;
-        string s;
-        cin>>s;
+    cin >> t;
 
-        map<char, int>mp;
-        for(int i=0; i<n; i++)
-        {
-            mp[s[i]]++;
-
-        }
-
-
+    for (int tc = 1; tc <= t; tc++) {
+        solve(tc);
     }
-    return 0;
 }
