5b202a0da26f2847d81cf398fc56e76e342146e9
Yanis Ahmed <yanisahmed12@gmail.com>
2024-12-18 22:39:28 +0600

    codechef contest added

---
+++ week8/day4/starter-165/problem_a.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define fastread()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+using namespace std;
+const int mod = 1e9 + 7;
+void solve()
+{
+
+}
+
+int main()
+{
+    fastread();
+    	int x;
+	cin>>x;
+	cout<<25-x<<endl;
+	return 0;
+}
+
